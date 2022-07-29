9db347585a113202ba1726c2fcaa699a582deb14
Daniel-Ugulino <danielugulino46@gmail.com>
2022-07-29 14:28:40 -0300

    alteração model de fisioterapia

--- model/fisioterapia.php
+++ model/fisioterapia.php
@@ -24,11 +24,13 @@ class fisioterapia{
     }
 
     public function getById(){
-        $stm = $this->conn->prepare("select id_consulta,fk_relatorio,resposta from consulta_fisio inner join questionario;");
+        $stm = $this->conn->prepare("
+        select f.id_consulta,q.fk_relatorio,pe.pergunta,q.resposta,q.obs,f.queixa,f.data_consulta from questionario q 
+        JOIN perguntas pe on q.fk_pergunta = pe.id_pergunta
+        JOIN consulta_fisio f on  q.fk_consulta = f.id_consulta WHERE q.fk_consulta is not null
+        ;");
         $stm->execute();
         return $stm->fetchAll(PDO::FETCH_OBJ);
     }
 
 }
-
-?>
