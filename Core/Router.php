22419d923029cf54767e383853ed9db80c549a08
Daniel-Ugulino <danielugulino46@gmail.com>
2022-08-09 13:55:23 -0300

    alteração no router

--- Core/Router.php
+++ Core/Router.php
@@ -1,22 +1,26 @@
 <?php
 
-require_once "controler/funcionario.php";
-require_once "controler/relatorio.php";
-require_once "controler/user.php";
-require_once "controler/log.php";
+require_once "../App/Controllers/funcionario.php";
+require_once "../App/Controllers/relatorio.php";
+require_once "../App/Controllers/log.php";
+require_once "../App/Controllers/user.php";
 
 class router
 {
-    
+    private $c_funcionario;
+    private $c_relatorio;
+    private $c_log;
+    private $c_user;
+
     public function __construct()
     {
-        
+        $this->c_funcionario = new funcionario_controler()    
     }
 
     function get_url()
     {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $url = strstr($uri, "med");
+        $url = strstr($uri, "Core");
         $url2 = strpbrk($url, "/");
         echo ($url2);
         switch ($url2) {
