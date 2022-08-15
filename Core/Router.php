b7165fae23f7962c686170c6c6042ef789cd8487
Daniel-Ugulino <danielugulino46@gmail.com>
2022-08-15 14:22:17 -0300

    adição de rotas para funcionario

--- Core/Router.php
+++ Core/Router.php
@@ -1,33 +1,38 @@
 <?php
 
-require_once "../App/Controllers/funcionario.php";
+require_once "../App/Controllers/Funcionario.php";
 require_once "../App/Controllers/relatorio.php";
 require_once "../App/Controllers/log.php";
 require_once "../App/Controllers/user.php";
 
-class router
+class Router
 {
-    private $c_funcionario;
+    private $funcionario;
     private $c_relatorio;
     private $c_log;
     private $c_user;
 
     public function __construct()
     {
-        $this->c_funcionario = new funcionario_controler()    
+        $this->funcionario = new funcionario_controler();
     }
 
-    function get_url()
+    public function get_url()
     {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $url = strstr($uri, "Core");
         $url2 = strpbrk($url, "/");
-        echo ($url2);
+        
         switch ($url2) {
             case "/teste":
                 echo ("a");
                 break;
-
+            case "/funcionario/cadastro":
+                $this->funcionario->cad_funcionario();
+                break;
+            case "/funcionario":
+                $this->funcionario->index();
+                break;
             default:
                 echo ("erro 404");
                 break;
