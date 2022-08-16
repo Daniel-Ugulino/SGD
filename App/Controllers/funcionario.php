825c2a32b4a901b5f78444e9b305ef0d794bf9a7
Daniel-Ugulino <danielugulino46@gmail.com>
2022-08-16 14:26:41 -0300

    ateração na funcão de cadastro de funcionario

--- App/Controllers/funcionario.php
+++ App/Controllers/funcionario.php
@@ -11,6 +11,7 @@ class report_controller
 
 class funcionario_controler
 {
+    private $error;
     private $atestado;
     private $exame;
     private $vacinas;
@@ -20,27 +21,37 @@ class funcionario_controler
     public function __construct()
     {
         $this->funcionario = new funcionario();
-        $this->funcionario->getAll();
     }
 
     public function index(){
-        $this->funcionario->getAll();
-        require_once dirname(__FILE__) ."\..\Views\Funcionario\index.php";
+        $funcionario = $this->funcionario->getAll();
+        if (isset($funcionario)){
+            require_once dirname(__FILE__) ."\..\Views\Funcionario\index.php";
+        }
+        else{
+            require_once dirname(__FILE__) ."\..\Views\Error\index.php";
+        }
     }
 
     public function cad_funcionario()
     {
         if ($_POST != null) {
             $data = json_decode(json_encode($_POST));
-            echo(json_encode($data));
+            echo (json_encode($data));
             $this->funcionario->nome = $data->nome;
             $this->funcionario->matricula = $data->matricula;
             $this->funcionario->cpf = $data->cpf;
             $this->funcionario->telefone = $data->telefone;
             $this->funcionario->email = $data->email;
+            $this->funcionario->nascimento = $data->nascimento;
             $this->funcionario->setor = $data->setor;
             $this->funcionario->fator_rh = $data->fator_rh;
-            $this->funcionario->insert();
+            if ($this->error != "") {
+                $this->funcionario->insert();
+            } else {
+                $this->error = $this->funcionario->insert();
+                require_once dirname(__FILE__) . "\..\Views\Error\index.php";
+            }
         }
         require_once dirname(__FILE__) . "\..\Views\Funcionario\cadastro.php";
     }
