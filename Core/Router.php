e8f005090d82658b1fc16d50a9754958a3a084c4
Daniel-Ugulino <danielugulino46@gmail.com>
2022-08-17 14:28:21 -0300

    adição de novas rotas

--- Core/Router.php
+++ Core/Router.php
@@ -7,31 +7,34 @@ require_once "../App/Controllers/user.php";
 
 class Router
 {
-    private $funcionario;
+    private $funcionario_controller;
     private $c_relatorio;
     private $c_log;
-    private $c_user;
+    private $user_controller;
 
     public function __construct()
     {
-        $this->funcionario = new funcionario_controler();
+        $this->funcionario_controller = new funcionario_controler();
+        $this->user_controller = new user_controller();
     }
 
     public function get_url()
     {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $url = strstr($uri, "public");
+        $url = strstr($uri, "med");
         $url2 = strpbrk($url, "/");
-        
         switch ($url2) {
             case "/teste":
                 echo ("a");
                 break;
-            case "/funcionario/cadastro":
-                $this->funcionario->cad_funcionario();
+            case "/funcionarios/cadastro":
+                $this->funcionario_controller->cad_funcionario();
                 break;
-            case "/funcionario":
-                $this->funcionario->index();
+            case "/funcionarios":
+                $this->funcionario_controller->index();
+                break;
+            case "/login":
+                $this->user_controller->create_user();
                 break;
             default:
                 echo ("erro 404");
