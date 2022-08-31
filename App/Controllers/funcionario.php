82b17f3e9e454485481034499cb478ec92b2a1cb
Daniel-Ugulino <danielugulino46@gmail.com>
2022-08-31 14:09:22 -0300

    alteração no controler e search e de cadastro

--- App/Controllers/funcionario.php
+++ App/Controllers/funcionario.php
@@ -1,15 +1,15 @@
 <?php
 
 require_once dirname(__FILE__) . "/../Models/Funcionario.php";
-require_once dirname(__FILE__) . "/../Models/Vacinas.php";
-require_once dirname(__FILE__) . "/../Models/Exame.php";
-require_once dirname(__FILE__) . "/../Models/Atestados.php";
+// require_once dirname(__FILE__) . "/../Models/Vacinas.php";
+// require_once dirname(__FILE__) . "/../Models/Exame.php";
+// require_once dirname(__FILE__) . "/../Models/Atestados.php";
 
 class funcionario_controler
 {
     private $error;
     private $funcionario_model;
-
+    private $funcionarios;
     public function __construct()
     {
         $this->funcionario_model = new funcionario();
@@ -18,19 +18,21 @@ class funcionario_controler
 
     private function injection_analyze($string)
     {
-        $string = htmlspecialchars($string);
-        $string = preg_replace('/[\!@#$%^&*()+=\-\[\]\';,.\/{}|":<>?~\\\\]/', "", $string);
-        $string = str_replace(["UPDATE", "DELETE", "DROP", "SELECT", "INSERT", "ALTER", "TABLE", "VIEW", "PROCEDURE", "CREATE", "DATABASE", "LIKE", "USE"], "", $column);
-        return $string;
+        $string1 = htmlspecialchars($string);
+        $string2 = preg_replace('/[\!@#$%^&*()+=\-\[\]\';,.\/{}|":<>?~\\\\]/', "", $string1);
+        $string3 = str_replace(["UPDATE", "DELETE", "DROP", "SELECT", "INSERT", "ALTER", "TABLE", "VIEW", "PROCEDURE", "CREATE", "DATABASE", "LIKE", "USE"], "", $string2);
+        return $string3;
     }
 
     public function index()
     {
-        $funcionarios = $this->funcionario_model->getAll();
-        if (isset($funcionarios)) {
-            require_once dirname(__FILE__) . "/../Views/Funcionario/index.php";
-        } else {
-            require_once dirname(__FILE__) . "/../Views/Error/index.php";
+        if (isset($_POST['search']) == null || $_POST['search'] == "") {
+            $this->funcionarios = $this->funcionario_model->getAll();
+            if (isset($this->funcionarios)) {
+                require dirname(__FILE__) . "/../Views/Funcionario/index.php";
+            } else {
+                require dirname(__FILE__) . "/../Views/Error/index.php";
+            }
         }
     }
 
@@ -40,19 +42,19 @@ class funcionario_controler
             if ($_POST != null) {
                 $data = json_decode(json_encode($_POST));
 
-                $this->funcionario_model->nome = $data->nome;
-                $this->funcionario_model->matricula = $data->matricula;
-                $this->funcionario_model->cpf = $data->cpf;
-                $this->funcionario_model->telefone = $data->telefone;
-                $this->funcionario_model->email = $data->email;
-                $this->funcionario_model->nascimento = $data->nascimento;
-                $this->funcionario_model->setor = $data->setor;
-                $this->funcionario_model->fator_rh = $data->fator_rh;
+                // $this->funcionario_model->nome = $data->nome;
+                // $this->funcionario_model->matricula = $data->matricula;
+                // $this->funcionario_model->cpf = $data->cpf;
+                // $this->funcionario_model->telefone = $data->telefone;
+                // $this->funcionario_model->email = $data->email;
+                // $this->funcionario_model->nascimento = $data->nascimento;
+                // $this->funcionario_model->setor = $data->setor;
+                // $this->funcionario_model->fator_rh = $data->fator_rh;
 
-                // $columns = array_columns((array)$data);
-                // foreach ($columns as $column) {
-                //     $this->funcionario_model->$column = $data->$column;
-                // }
+                $columns = array_keys((array)$data);
+                foreach ($columns as $column) {
+                    $this->funcionario_model->$column = $data->$column;
+                }
 
                 try {
                     $this->funcionario_model->insert();
@@ -90,13 +92,17 @@ class funcionario_controler
 
     public function search_funcionario()
     {
-        $data = json_decode((json_encode($_POST)));
-        $column = $this->injection_analyze($data->column);
-        $funcionarios = $this->funcionario_model->search($column, $data->field);
-        if (isset($funcionarios)) {
-            require_once dirname(__FILE__) . "/../Views/Funcionario/index.php";
-        } else {
-            require_once dirname(__FILE__) . "/../Views/Error/index.php";
+        if (isset($_POST["search"])) {
+            $data = json_decode((json_encode($_POST)));
+            $column = $this->injection_analyze($data->column);
+            $field = $this->injection_analyze($data->field);
+            unset($this->funcionarios);
+            $this->funcionarios = $this->funcionario_model->search($column, $field);
+            if (isset($this->funcionarios)) {
+                echo (json_encode($this->funcionarios));
+            } else {
+                require_once dirname(__FILE__) . "/../Views/Error/index.php";
+            }
         }
     }
 }
